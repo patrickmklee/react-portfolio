bf257e51608e906b5bb7bd736740f35d8e487b52
patrickmklee <patrickmklee93@gmail.com>
2021-05-23 00:53:01 -0700

    Fixed comma

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
